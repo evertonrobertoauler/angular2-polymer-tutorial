86901c0f3750eabe38756280dc2e545e4040239f
Everton R. Auler <evertonrobertoauler@gmail.com>
2016-06-23 21:09:18 -0300

    Adding a alternative for production

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -13,6 +13,14 @@ module.exports = function(defaults) {
       'rxjs/**/*.+(js|js.map)',
       '@angular/**/*.+(js|js.map)',
       '@vaadin/**/*.+(js|js.map)'
+    ],
+    polyfills: [
+      'vendor/es6-shim/es6-shim.js',
+      'vendor/reflect-metadata/Reflect.js',
+      'vendor/systemjs/dist/system.src.js',
+      'vendor/zone.js/dist/zone.js',
+      'bower_components/webcomponentsjs/webcomponents-lite.js',
+      'bower_components/vaadin-grid/vaadin-grid.min.js'
     ]
   });
 };
---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var vulcanize = require('gulp-vulcanize');
+var rm = require('gulp-rm');
+
+gulp.task('vulcanize', function () {
+  return gulp.src('src/elements.html')
+    .pipe(vulcanize({
+      abspath: '',
+      excludes: [],
+      stripExcludes: false,
+      stripComments: true,
+      inlineScripts: true,
+      inlineCss: true
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['vulcanize'], function () {
+  gulp.watch('dist/index.html', ['vulcanize']);
+  gulp.watch('src/elements.html', ['vulcanize']);
+});
+
+gulp.task('bower_components:rm:ts', function () {
+  return gulp.src('src/bower_components/**/*.ts', { read: false }).pipe(rm())
+});
